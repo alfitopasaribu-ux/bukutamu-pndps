5ed2035af8bfb39a3e1c8e542343e20103bb0209
Alfitopasaribu <alfitopasaribu@gmail.com>
2026-05-15 16:14:29 +0800

    fix: prisma generate in build + disable eslint

--- eslint.config.mjs
+++ eslint.config.mjs
@@ -1,17 +1,5 @@
-import { dirname } from "path";
-import { fileURLToPath } from "url";
-import { FlatCompat } from "@eslint/eslintrc";
-
-const __filename = fileURLToPath(import.meta.url);
-const __dirname = dirname(__filename);
-
-const compat = new FlatCompat({
-  baseDirectory: __dirname,
-});
-
-const eslintConfig = [
-  ...compat.extends("next/core-web-vitals", "next/typescript"),
-];
-
+/** @type {import('eslint').Linter.Config[]} */
+const eslintConfig = [];
 export default eslintConfig;
+
 
